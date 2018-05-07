19990e3b770578629286bfedc76700f62dc190bf
romaneplr <romane.pellerin@u-psud.fr>
2018-05-07 14:46:36 +0200

    ajout image

--- AXS/app/src/main/java/com/axsavaro/axsavaro/MainActivity.java
+++ AXS/app/src/main/java/com/axsavaro/axsavaro/MainActivity.java
@@ -1,13 +1,30 @@
 package com.axsavaro.axsavaro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView compte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.compte = (ImageView) findViewById(R.id.compte);
+
+        compte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherAct = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(otherAct);
+                finish();
+            }
+        });
     }
 }
